768d354ee8d966c93465ff9bb2d7da27ad3e1baa
Esteban-Rosas <estebanrosas94@gmail.com>
2020-12-17 23:51:21 -0500

    Prueba

--- DJANGO/TrainingApp/TrainingApp/urls.py
+++ DJANGO/TrainingApp/TrainingApp/urls.py
@@ -1,4 +1,4 @@
-"""TrainingApp URL Configuration
+"""TrainningApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('warehouse/', include('warehouse.urls')),
+    path('market/', include('market.urls'))
 ]
